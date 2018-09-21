4d3539f9268944b1d28ed4cc9c62739c60e4c422
heelong <hel10401@163.com>
2018-09-21 18:37:29 +0800

    目标提取算法

--- Test_curve.cpp
+++ Test_curve.cpp
@@ -2,12 +2,16 @@
 #include "cv.h"
 #include <time.h>
 #include <windows.h>
-void main()
 #include"opencv2/opencv.hpp"
 #include <iostream>
 #include <fstream>
 #include <iostream>
 #include <sstream>
+
+
+#include <vector>
+#include <stack>
+
 using namespace cv;
 using namespace std;
 void curveFit()
@@ -29,26 +33,6 @@ void curveFit()
 			y_ += ((double)rand() / RAND_MAX)*(b - a) + a;
 			points.push_back(cv::Point2d(x_, y_));
 		}
-		
-		//points.push_back(cv::Point(110., 60.));
-		//points.push_back(cv::Point(150., 70.));
-		//points.push_back(cv::Point(200., 90.));
-		//points.push_back(cv::Point(220., 95.));
-		//points.push_back(cv::Point(230., 105.));
-		//points.push_back(cv::Point(252., 140.));
-		//points.push_back(cv::Point(301., 200.));
-		//points.push_back(cv::Point(300., 220.));
-		//points.push_back(cv::Point(350., 400.));
-		//points.push_back(cv::Point(110., 48.));
-		//points.push_back(cv::Point(117., 68.));
-		//points.push_back(cv::Point(140., 60.));
-		//points.push_back(cv::Point(212., 98.));
-		//points.push_back(cv::Point(243., 90.));
-		//points.push_back(cv::Point(233., 115.));
-		//points.push_back(cv::Point(272., 120.));
-		//points.push_back(cv::Point(321., 240.));
-		//points.push_back(cv::Point(330., 210.));
-		//points.push_back(cv::Point(342., 370.));
 
 		//将拟合点绘制到空白图上  
 		for (int i = 0; i < points.size(); i++)
@@ -169,59 +153,234 @@ int minAreaRectTest()
 		//circle(img, center, cvRound(radius), Scalar(0, 255, 255), 1, CV_AA);
 		imshow("j", img);
 		cv::waitKey(300);
-		//char key = (char)waitKey();
-		//if (key == 27 || key == 'q' || key == 'Q')
-		//	break;
 
 	}
 	return 0;
 }
+
+
+
+
+
+/*
+Input:
+src: 待检测连通域的二值化图像
+Output:
+dst: 标记后的图像
+featherList: 连通域特征的清单
+return：
+连通域数量。
+*/
+int bwLabel(Mat & src, Mat & dst, vector<Feather> & featherList)
+{
+	int rows = src.rows;
+	int cols = src.cols;
+
+	int labelValue = 0;
+	Point seed, neighbor;
+	stack<Point> pointStack;    // 堆栈
+
+	int area = 0;               // 用于计算连通域的面积
+	int leftBoundary = 0;       // 连通域的左边界，即外接最小矩形的左边框，横坐标值，依此类推
+	int rightBoundary = 0;
+	int topBoundary = 0;
+	int bottomBoundary = 0;
+	Rect box;                   // 外接矩形框
+	Feather feather;
+
+	featherList.clear();    // 清除数组
+
+	dst.release();
+	dst = src.clone();
+	for (int i = 0; i < rows; i++)
+	{
+		uchar *pRow = dst.ptr<uchar>(i);
+		for (int j = 0; j < cols; j++)
+		{
+			if (pRow[j] == 255)
+			{
+				area = 0;
+				labelValue++;           // labelValue最大为254，最小为1.
+				seed = Point(j, i);     // Point（横坐标，纵坐标）
+				dst.at<uchar>(seed) = labelValue;
+				pointStack.push(seed);
+
+				area++;
+				leftBoundary = seed.x;
+				rightBoundary = seed.x;
+				topBoundary = seed.y;
+				bottomBoundary = seed.y;
+
+				while (!pointStack.empty())
+				{
+					neighbor = Point(seed.x + 1, seed.y);
+					if ((seed.x != (cols - 1)) && (dst.at<uchar>(neighbor) == 255))
+					{
+						dst.at<uchar>(neighbor) = labelValue;
+						pointStack.push(neighbor);
+
+						area++;
+						if (rightBoundary < neighbor.x)
+							rightBoundary = neighbor.x;
+					}
+
+					neighbor = Point(seed.x, seed.y + 1);
+					if ((seed.y != (rows - 1)) && (dst.at<uchar>(neighbor) == 255))
+					{
+						dst.at<uchar>(neighbor) = labelValue;
+						pointStack.push(neighbor);
+
+						area++;
+						if (bottomBoundary < neighbor.y)
+							bottomBoundary = neighbor.y;
+
+					}
+
+					neighbor = Point(seed.x - 1, seed.y);
+					if ((seed.x != 0) && (dst.at<uchar>(neighbor) == 255))
+					{
+						dst.at<uchar>(neighbor) = labelValue;
+						pointStack.push(neighbor);
+
+						area++;
+						if (leftBoundary > neighbor.x)
+							leftBoundary = neighbor.x;
+					}
+
+					neighbor = Point(seed.x, seed.y - 1);
+					if ((seed.y != 0) && (dst.at<uchar>(neighbor) == 255))
+					{
+						dst.at<uchar>(neighbor) = labelValue;
+						pointStack.push(neighbor);
+
+						area++;
+						if (topBoundary > neighbor.y)
+							topBoundary = neighbor.y;
+					}
+
+					seed = pointStack.top();
+					pointStack.pop();
+				}
+				box = Rect(leftBoundary, topBoundary, rightBoundary - leftBoundary, bottomBoundary - topBoundary);
+				//rectangle(src, box, 255);
+				feather.area = area;
+				feather.boundingbox = box;
+				feather.label = labelValue;
+				featherList.push_back(feather);
+			}
+		}
+	}
+	return labelValue;
+}
+
+int ConnectedComponent(Mat& src)
+{
+	if (src.empty())
+		exit(-1);
+	threshold(src, src, 127, 255, THRESH_BINARY);   // 二值化图像
+	vector<Feather> featherList;                    // 存放连通域特征
+	Mat dst;
+	cout << "连通域数量： " << bwLabel(src, dst, featherList) << endl;
+
+	// 为了方便观察，可以将label“放大”
+	for (int i = 0; i < dst.rows; i++)
+	{
+		uchar *p = dst.ptr<uchar>(i);
+		for (int j = 0; j < dst.cols; j++)
+		{
+			p[j] = 30 * p[j];
+		}
+	}
+
+	cout << "标号" << "\t" << "面积" << endl;
+	for (vector<Feather>::iterator it = featherList.begin(); it < featherList.end(); it++)
+	{
+		cout << it->label << "\t" << it->area << endl;
+		rectangle(dst, it->boundingbox, 255);
+	}
+
+	imshow("src", src);
+	imshow("dst", dst);
+
+	cv::waitKey(100);
+	return 0;
+}
+
+void ImageBasedObjDector(int file)
+{
+	stringstream ss;
+	ss << "../data/" << file << "_LiDAR.txt";
+
+	std::ifstream in_file_(ss.str(), std::ifstream::in);
+	if (!in_file_.is_open()) {
+		std::cerr << "Cannot open input file: " << ss.str() << std::endl;
+		return;
+	}
+	std::string line;
+	vector<PointsXYZInt>PointCloud; PointCloud.reserve(50000);
+	getline(in_file_, line);
+	PointsXYZInt tmp;
+	while (getline(in_file_, line))
+	{
+		std::istringstream iss(line);
+		iss >> tmp.x;
+		iss >> tmp.y;
+		iss >> tmp.z;
+		iss >> tmp.index;
+		iss >> tmp.horizontal_angle;
+		iss >> tmp.intensity;
+		iss >> tmp.ClassNum;
+		iss >> tmp.Distance;
+		iss >> tmp.Range_meter;
+		iss >> tmp.Laser_Line_Number;
+		iss >> tmp.sensorCount;
+		PointCloud.push_back(tmp);
+	}
+	const int pointNum = PointCloud.size();
+	int *Pixnum_02m = new int[pointNum]();  //0.2m 分辨率 
+	int imgx = 0, imgy = 0;
+	std::vector<cv::Point2d> pointsIndex; pointsIndex.resize(pointNum);
+	for (int i = 0; i < pointNum; i += 1){
+		imgx = pointsIndex[i].x = PointCloud[i].x / 20 + 500;//以1分米为单位
+		imgy = pointsIndex[i].y = PointCloud[i].y / 20 + 500;
+		Pixnum_02m[i] = imgy * 1000 + imgx;
+	}
+
+	//创建用于绘制的深蓝色背景图像
+	cv::Mat image = cv::Mat::zeros(1000, 1000, CV_8UC1);
+	image.setTo(cv::Scalar(0));
+	//将点云投影到空白图上  
+	for (int i = 0; i < pointNum; i++)
+	{
+		image.at <uchar>(Pixnum_02m[i]) = 255;
+	}
+	cv::imshow("image", image);
+	//
+	int SizeOfKernel = 5;
+	cv::Mat kernel = cv::getStructuringElement(cv::MORPH_RECT, Size(SizeOfKernel, SizeOfKernel));
+	cv::Mat dst1, dis2;
+	//先膨胀
+	cv::dilate(image, dst1, kernel);
+	cv::imshow("result1", dst1);
+	//再腐蚀
+	SizeOfKernel = 5;
+	kernel = cv::getStructuringElement(cv::MORPH_RECT, Size(SizeOfKernel, SizeOfKernel));
+	cv::erode(dst1, dis2, kernel);
+	cv::imshow("result2", dis2);
+
+	//ConnectedComponent(dst1);
+	cv::waitKey(100);
+	//cv::waitKey(0);
+	PointCloud.clear();
+	pointsIndex.clear();
+	delete[]Pixnum_02m;
+	return;
+
+}
+
 int main(int argc, char** argv[])
 {
-	Mat img(500, 500, CV_8UC3);
-
-	std::string in_file_name_ = "D:\\workspace\\baseline-log\\obstacleTracker\\68_Radar.txt";
-	std::ifstream in_file_(in_file_name_.c_str(), std::ifstream::in);
-
-	vector<Point2f>points; points.reserve(1000);
-	std::string line_;
-	int mark[100]={-1};
-	int i;
-	while (getline(in_file_, line_)) {
-		std::istringstream iss(line_);
-		Point2d pt; 
-		iss >> pt.x; iss >> pt.y; iss >> i;
-		pt.x += 250.0; pt.y += 250.0;
-		points.push_back(pt);
-		mark[i]++;
-	}
-	int count = 0;
-	for (int j = 1; j <= i;j++)
-	{
-		vector<Point>points1;
-		for (int k = 0; k < mark[j];k++)
-		{
-			
-			points1.push_back(points[count+k]);
-		}
-		count += mark[j];
-		RotatedRect box = minAreaRect(Mat(points));//点集的最小外接旋转矩形
-		Point2f tr[4], center;
-		float radius = 0;
-		box.points(tr);
-		minEnclosingCircle(Mat(points), center, radius);//点集的最小外接圆
-		img = Scalar::all(0);
-		for (int ic = 0; ic < count; ic++)
-		{
-			circle(img, points[ic], 3, Scalar(0, 0, 255), CV_FILLED, CV_AA);
-		}
-		for (int ic = 0; ic < 4; ic++)
-		{
-			line(img, tr[ic], tr[(ic + 1) % 4], Scalar(0, 255, 0), 1, CV_AA);
-		}
-		//circle(img, center, cvRound(radius), Scalar(0, 255, 255), 1, CV_AA);
-		imshow("j", img);
-		cv::waitKey(300);
-	}
+	for (int i = 1; i < 6733;i++)
+		ImageBasedObjDector(i);
 	return 0;
 }
--- curve.h
+++ curve.h
@@ -11,9 +11,33 @@
 #include <time.h>
 #include "cv.h"
 #include "highgui.h"
-
+#include "curve.h"
+#include <time.h>
+#include <windows.h>
+#include"opencv2/opencv.hpp"
 using namespace std;
+struct PointsXYZInt
+{
+	int x; 
+	int y;
+	int z;
+	int index;
+	int horizontal_angle;
+	int intensity; 
+	int ClassNum;
+	int Distance;
+	int Laser_Line_Number;
+	int sensorCount;
+	float Range_meter;
+};
 
+
+typedef struct _Feather
+{
+	int label;              // 连通域的label值
+	int area;               // 连通域的面积
+	cv::Rect boundingbox;       // 连通域的外接矩形框
+} Feather;
 bool polynomial_curve_fit(std::vector<cv::Point2d>& key_point, int n, cv::Mat& A);
 
 #endif
